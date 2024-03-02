fd21dcfcbb100b4721a1a37532a2261d244eedcc
unknown <arridohfuad@gmail.com>
2024-03-02 16:52:20 +0900

    first commit

---
+++ Exercise.py
@@ -0,0 +1,10 @@
+name = input()
+age = int(input())
+weight = input()
+height = input()
+
+print ("\nNama Saya :", name)
+print (f"Umur :{age} Tahun")
+print (f"Berat Badan : {weight} Kg")
+print (f"Tinggi Badan : {height} cm")
+print(type(age))
---
+++ Penggajian.py
@@ -0,0 +1,27 @@
+nama = input('Masukkan nama anda : ')
+hari_kerja = int(input('Masukkan kehadiran : '))
+
+# Tetapkan kehadiran perbulan & gaji
+hari_kerja_perbulan = 24
+gaji = 3500000
+
+# setiap 1 hari lembur mendapatkan gaji tambahan Rp.150,000
+# ketika hari kerja lebih dari hari kerja perbulan
+# maka tambahan gaji hanya didapat dari gaji lembur
+if hari_kerja > hari_kerja_perbulan :
+    harilembur = hari_kerja - hari_kerja_perbulan
+    lembur = (harilembur)* 150000
+    hari_kerja1 = hari_kerja
+    hari_kerja1 = hari_kerja_perbulan    
+else :
+    lembur = 0
+
+Totalgaji= int((hari_kerja1/hari_kerja_perbulan)*gaji + lembur)
+Totalgaji = f"{Totalgaji:,}"
+print("\nNama :", nama)
+print(f"Kehadiran anda : {hari_kerja} hari")
+if lembur > 0:
+    print(f"Anda lembur selama {harilembur} hari.")
+else :
+    print("Anda tidak lembur")
+print(f"gaji yang didapatkan {nama} adalah Rp.", Totalgaji)
